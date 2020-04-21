a51f4401cfc53faa44965d455d631cf031dd9e8d
Ubuntu <ubuntu@ip-172-31-35-111.us-east-2.compute.internal>
2020-04-21 08:41:05 +0000

    Fixed rubocop offenses

--- enumerables.rb
+++ enumerables.rb
@@ -1,104 +1,112 @@
-
 def block_method(check, value, arr)
-    arr.push(value) if check
+  arr.push(value) if check
 end
 
- def param_method(value, param, arr)
-    arr.push(value) if param == value
- end
- 
- def reg_class_all(param, arr, value)
-    if param.is_a?(Class)
-      arr.push(value) unless !value.is_a?(param)
-    elsif param.is_a?(Regexp) 
-      if param == /d/ && value.is_a?(String)
-        arr.push(value) 
-      else
-        arr
-      end
-    else
-      arr
-    end
- end
- 
-  def reg_class_any(param, arr, value)
-    if param.is_a?(Class)
-      arr.push(value) unless !value.is_a?(param)
-    elsif param == /d/
+def param_method(value, param, arr)
+  arr.push(value) if param == value
+end
+
+def reg_class_all(param, arr, value)
+  if param.is_a?(Class)
+    arr.push(value) if value.is_a?(param)
+  elsif param.is_a?(Regexp)
+    if param.match(value) && value.is_a?(String)
       arr.push(value)
     else
       arr
     end
+  elsif param == value
+    arr.push(value)
+  else
+    arr
   end
-  
-  def reg_class_none(param, arr, value)
-    if param.is_a?(Class)
-      arr.push(value) unless value.is_a?(param)
-    else
-      arr.push(value) unless param == /t/ || param == value
-    end
-  end
-  
-  def addition(x, y)
-    result = x + y
-    result
-  end
-  
-  def production(x, y)
-    result = x * y
-    result
-  end
-  
-  def subtraction(x,y)
-    result = x - y
-    result
-  end 
-  
-   def division(x,y)
-    result = x / y
-    result
-   end 
-  
-  def accumulator(total, acc_arr,ind_arr)
-    i = 2
-    first_total = method(total).call(ind_arr[0],ind_arr[1])
-    acc_arr.push(first_total)
-    while i < ind_arr.length
-      j = i - 2
-      x = ind_arr[i]
-      result = method(total).call(acc_arr[j],x)
-      acc_arr.push(result)
-      i += 1
-    end
-    acc_arr
-  end
-  
-  def accumulator_with_initial(total, acc_arr, ind_arr, first)
-    i = 1
-    first_total =  method(total).call(first,ind_arr[0])
-    acc_arr.push(first_total)
-    while i < ind_arr.length
-      j = i - 1
-      x = ind_arr[i]
-      result = method(total).call(acc_arr[j],x)
-      acc_arr.push(result)
-      i += 1
-    end
-    acc_arr
-  end
+end
 
+def reg_class_any(param, arr, value)
+  if param.is_a?(Class)
+    arr.push(value) if value.is_a?(param)
+  elsif param.is_a?(Regexp)
+    if param.match(value) && value.is_a?(String)
+      arr.push(value)
+    else
+      arr
+    end
+  elsif param == value
+    arr.push(value)
+  else
+    arr
+  end
+end
+
+def reg_class_none(param, arr, value)
+  if param.is_a?(Class)
+    arr.push(value) unless value.is_a?(param)
+  elsif param.is_a?(Regexp)
+    arr.push(value) unless param.match(value) && value.is_a?(String)
+  else
+    arr.push(value) unless param == value
+  end
+end
+
+def addition(val1, val2)
+  result = val1 + val2
+  result
+end
+
+def production(val1, val2)
+  result = val1 * val2
+  result
+end
+
+def subtraction(val1, val2)
+  result = val1 - val2
+  result
+end
+
+def division(val1, val2)
+  result = val1 / val2
+  result
+end
+
+def accumulator(total, acc_arr, ind_arr)
+  i = 2
+  first_total = method(total).call(ind_arr[0], ind_arr[1])
+  acc_arr.push(first_total)
+  while i < ind_arr.length
+    j = i - 2
+    x = ind_arr[i]
+    result = method(total).call(acc_arr[j], x)
+    acc_arr.push(result)
+    i += 1
+  end
+  acc_arr
+end
+
+def accumulator_with_initial(total, acc_arr, ind_arr, first)
+  i = 1
+  first_total = method(total).call(first, ind_arr[0])
+  acc_arr.push(first_total)
+  while i < ind_arr.length
+    j = i - 1
+    x = ind_arr[i]
+    result = method(total).call(acc_arr[j], x)
+    acc_arr.push(result)
+    i += 1
+  end
+  acc_arr
+end
 
 module MyEnumerables
   include Enumerable
   def my_each
     i = 0
-    self.is_a?(Range)? self_arr = self.to_a : self_arr = self
+    self_arr = is_a?(Range) ? to_a : self
     if block_given?
-    while i < self_arr.length
-      x = self_arr[i]
-      yield(x)
-      i += 1
-    end
+      while i < self_arr.length
+        x = self_arr[i]
+        yield(x)
+        i += 1
+      end
     else
       to_enum(:each)
     end
@@ -106,13 +114,13 @@ module MyEnumerables
 
   def my_each_with_index
     i = 0
-    self.is_a?(Range)? self_arr = self.to_a : self_arr = self
+    self_arr = is_a?(Range) ? to_a : self
     if block_given?
-    while i < self_arr.length
-      x = self_arr[i]
-      yield(x, i)
-      i += 1
-    end
+      while i < self_arr.length
+        x = self_arr[i]
+        yield(x, i)
+        i += 1
+      end
     else
       to_enum(:each_with_index)
     end
@@ -120,56 +128,56 @@ module MyEnumerables
 
   def my_select
     new_arr = []
-    self.is_a?(Range)? self_arr = self.to_a : self_arr = self
+    self_arr = is_a?(Range) ? to_a : self
     if block_given?
-    self_arr.my_each do |x|
-      check = yield(x)
-      block_method(check, x, new_arr)
-    end
-    new_arr
+      self_arr.my_each do |x|
+        check = yield(x)
+        block_method(check, x, new_arr)
+      end
+      new_arr
     else
       to_enum(:my_select)
     end
   end
 
-  def my_all?(arg=nil)
+  def my_all?(arg = nil)
     new_arr = []
-    self.is_a?(Range)? self_arr = self.to_a : self_arr = self
+    self_arr = is_a?(Range) ? to_a : self
     self_arr.my_each do |x|
       if (arg && block_given?) || arg
-       reg_class_all(arg,new_arr,x)
-      elsif  block_given? 
+        reg_class_all(arg, new_arr, x)
+      elsif block_given?
         check = yield(x)
-        block_method(check,x, new_arr)
+        block_method(check, x, new_arr)
       else new_arr.push(x) unless x.nil? || x == false
       end
     end
-    puts "Warning: given block not used" if arg && block_given?
+    puts 'Warning: given block not used' if arg && block_given?
     new_arr.length == self_arr.length
   end
 
-  def my_any?(arg=nil)
+  def my_any?(arg = nil)
     new_arr = []
-    self.is_a?(Range)? self_arr = self.to_a : self_arr = self
+    self_arr = is_a?(Range) ? to_a : self
     self_arr.my_each do |x|
       if (arg && block_given?) || arg
-         reg_class_any(arg,new_arr,x)
+        reg_class_any(arg, new_arr, x)
       elsif block_given?
         yield(x)
         new_arr.push(x) if yield(x)
       else new_arr.push(x) unless x.nil? || x == false
       end
     end
-    puts "Warning: given block not used" if arg && block_given?
+    puts 'Warning: given block not used' if arg && block_given?
     !new_arr.empty?
   end
 
-  def my_none?(arg=nil)
+  def my_none?(arg = nil)
     new_arr = []
-    self.is_a?(Range)? self_arr = self.to_a : self_arr = self
+    self_arr = is_a?(Range) ? to_a : self
     self_arr.my_each do |x|
       if (arg && block_given?) || arg
-        reg_class_none(arg,new_arr,x)
+        reg_class_none(arg, new_arr, x)
       elsif block_given?
         yield(x)
         new_arr.push(x) unless yield(x)
@@ -182,7 +190,7 @@ module MyEnumerables
 
   def my_count(num = nil)
     new_arr = []
-    self.is_a?(Range)? self_arr = self.to_a : self_arr = self
+    self_arr = is_a?(Range) ? to_a : self
     self_arr.my_each do |x|
       if block_given?
         check = yield(x)
@@ -194,17 +202,17 @@ module MyEnumerables
     new_arr.length && num.nil? && !block_given? ? self_arr.length : new_arr.length
   end
 
-  def my_map(prc=nil)
+  def my_map(prc = nil)
     new_arr = []
-    self.is_a?(Range)? self_arr = self.to_a : self_arr = self
+    self_arr = is_a?(Range) ? to_a : self
     if (prc && block_given?) || prc
       self_arr.my_each do |x|
-      prc.call(x)
-      new_arr.push(prc.call(x))
-    end
-     new_arr
+        prc.call(x)
+        new_arr.push(prc.call(x))
+      end
+      new_arr
     elsif block_given?
-    self_arr.my_each do |x|
+      self_arr.my_each do |x|
         yield(x)
         new_arr.push(yield(x))
       end
@@ -213,21 +221,21 @@ module MyEnumerables
     end
   end
 
-  def my_inject(num = nil,val = nil)
+  def my_inject(num = nil, val = nil)
     total_arr = []
-    self.is_a?(Range)? self_arr = self.to_a : self_arr = self
+    self_arr = is_a?(Range) ? to_a : self
     if !block_given?
       if num && val
-        accumulator_with_initial(:addition, total_arr, self_arr, num) if val == :+ 
-        accumulator_with_initial(:production, total_arr, self_arr, num)  if val == :* 
-        accumulator_with_initial(:subtraction, total_arr, self_arr, num)  if val == :-
-        accumulator_with_initial(:division, total_arr, self_arr, num)  if val == :/ 
+        accumulator_with_initial(:addition, total_arr, self_arr, num) if val == :+
+        accumulator_with_initial(:production, total_arr, self_arr, num) if val == :*
+        accumulator_with_initial(:subtraction, total_arr, self_arr, num) if val == :-
+        accumulator_with_initial(:division, total_arr, self_arr, num) if val == :/
       end
       if num
-          accumulator(:addition,total_arr,self_arr) if num == :+
-          accumulator(:subtraction,total_arr,self_arr) if num == :-
-          accumulator(:production,total_arr,self_arr) if num == :*
-          accumulator(:division,total_arr,self_arr) if num == :/
+        accumulator(:addition, total_arr, self_arr) if num == :+
+        accumulator(:subtraction, total_arr, self_arr) if num == :-
+        accumulator(:production, total_arr, self_arr) if num == :*
+        accumulator(:division, total_arr, self_arr) if num == :/
       end
     else
       if num
@@ -246,15 +254,15 @@ module MyEnumerables
         first_total = yield(self_arr[0], self_arr[1])
         total_arr.push(first_total)
         while i < self_arr.length
-        j = i - 2
-        x = self_arr[i]
-        total = yield(x, total_arr[j])
-        total_arr.push(total)
-        i += 1
+          j = i - 2
+          x = self_arr[i]
+          total = yield(x, total_arr[j])
+          total_arr.push(total)
+          i += 1
         end
       end
     end
-     total_arr.pop
+    total_arr.pop
   end
 end
 
@@ -270,6 +278,5 @@ def multiply_els(arr)
   arr.my_inject { |product, el| product * el }
 end
 
-puts %w[will paul mary].my_any?(/d/) 
-puts %w[will paul mary].any?(/d/) 
-
+puts [1, 4, 5].my_inject(:-)
+puts [1, 4, 5].inject(:-)
